851d54d9f439d21dac506b98743c666d9ed23d2e
rupesh00mahat <rupesh00mahat@gmail.com>
2024-05-29 22:09:43 +0545

    create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
 import './style/global.css';
+import Sidebar from './components/sidebar';
 function App() {
 
   return (
     <>
         <Navbar/>
+        <div className='body-wrapper'>
+          <Sidebar/>
+        </div>
     </>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import '../style/navbar.css';
+import "../style/navbar.css";
+import { CiSearch } from "react-icons/ci";
+import { MdDarkMode } from "react-icons/md";
+
 
 function Navbar() {
-  return <nav id="navbar-wrapper">
-    <div className="navbar-title">
-        <h2>My wordspace</h2>
-    </div>
-    <div className="navbar-content">
-        <input type="text" placeholder="Search here..."/>
-        <div className="navbar-sidebar">
-            <button>Dark Mode</button>
-            <button>Upgrade</button>
+  return (
+    <nav id="navbar-wrapper">
+      <div className="navbar-title">
+        <h2>My workspace</h2>
+      </div>
+      <div className="navbar-content">
+        <div className="navbar-input-wrapper">
+          <CiSearch size={30} color="#4f525a" />
+
+          <input type="text" placeholder="Search here..." />
         </div>
-    </div>
-  </nav>;
+        <div className="navbar-sidebar">
+          <button className="dark-mode-toggle">
+            <MdDarkMode size={30} color="#6f737f"/>
+          </button>
+          <button className="upgrade-button">Upgrade</button>
+
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
