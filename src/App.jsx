707c439047f69aec69da43490c28d19ff7a3ebf9
rupesh00mahat <rupesh00mahat@gmail.com>
2024-07-19 21:01:34 +0545

    delete unnecessary files and css and code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-import Navbar from "./components/navbar";
-import "./style/global.css";
-import ContentArea from "./components/content-area";
-import { useState } from "react";
-import Default from "./components/default";
+
 function App() {
-  const [showDefault, setShowDefault] = useState(false);
-  return (
-    <>
-      {showDefault ? (
-        <Default />
-      ) : (
-        <>
-          <Navbar />
-          <div className="body-wrapper">
-            {/* <Sidebar /> */}
-            <ContentArea />
-          </div>
-        </>
-      )}
-    </>
-  );
+ <></>
 }
 
 export default App;
