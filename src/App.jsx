ee05d17c39ea27355c1b397448fef39d940121b8
rupesh00mahat <rupesh00mahat@gmail.com>
2024-07-23 20:51:04 +0545

    add react-beautiful-dnd

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import ListBackground from "./components/list-bg";
 import Navbar from "./components/navbar";
 import './style/global.css';
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+const itemsFromBackEnd = [{id: 0, task: 'Enter the gym'}, {id: 1, task: 'Hit deadlift'}];
+
 function App() {
 
     const [task, newTask] = useState([]);
+    const [columns, setColumns] = useState(itemsFromBackEnd)
 
     const createNewTask = (e) =>{
         e.preventDefault();
@@ -16,6 +21,30 @@ function App() {
  <button
  onClick={createNewTask}
  >Create Task</button>
+ <DragDropContext onDragEnd={result => console.log(result)}>
+    {Object.entries(columns).map(([id, columns])=>{
+        return (
+            <Droppable droppableId={id}>
+                {(provided, snapshot)=>{
+                    return (
+                        <div {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{background: snapshot.isDraggingOver ? 'lightblue': 'lightgreen',
+                            padding: 4,
+                            width: 200,
+                            minHeight: 500,
+
+                        }}
+                        >
+
+                        </div>
+
+                    )
+                }}
+            </Droppable>
+        );
+    })}
+ </DragDropContext>
 <div className="container list-background-wrapper flex justify-between">
     <ListBackground tasks={task}/>
     <ListBackground/>
