ede2e9054a52ffb756716f023c6dd72092b9b736
rupesh00mahat <rupesh00mahat@gmail.com>
2024-07-25 20:52:15 +0545

    add lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ListBackground from "./components/list-bg";
 import Navbar from "./components/navbar";
 import './style/global.css';
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
-const itemsFromBackEnd = [{id: 0, task: 'Enter the gym'}, {id: 1, task: 'Hit deadlift'}];
 
 function App() {
 
     const [task, newTask] = useState([]);
-    const [columns, setColumns] = useState(itemsFromBackEnd)
+    const inputTask = useRef("");
 
     const createNewTask = (e) =>{
         e.preventDefault();
-        newTask([...task, 'Hit the gym']);
+       newTask([...task,inputTask.current.value]);
+       inputTask.current.value  = "";
     }
 
  return <>
  <Navbar/> 
+    <input type="text" name="task" id="task" ref={inputTask} />
  <button
  onClick={createNewTask}
  >Create Task</button>
- <DragDropContext onDragEnd={result => console.log(result)}>
-    {Object.entries(columns).map(([id, columns])=>{
-        return (
-            <Droppable droppableId={id}>
-                {(provided, snapshot)=>{
-                    return (
-                        <div {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{background: snapshot.isDraggingOver ? 'lightblue': 'lightgreen',
-                            padding: 4,
-                            width: 200,
-                            minHeight: 500,
-
-                        }}
-                        >
-
-                        </div>
-
-                    )
-                }}
-            </Droppable>
-        );
-    })}
- </DragDropContext>
+ 
 <div className="container list-background-wrapper flex justify-between">
-    <ListBackground tasks={task}/>
-    <ListBackground/>
-    <ListBackground/>
+    <ListBackground title="Not Started" tasks={task}/>
+    <ListBackground title="Ongoing"/>
+    <ListBackground title="Finished"/>
 </div>
  </>
 }
--- src/components/list-bg.jsx
+++ src/components/list-bg.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import "../style/list.css";
+import { MdDelete } from "react-icons/md";
 
-function ListBackground({tasks}) {
+function ListBackground({ tasks, title }) {
   return (
-    <ul id='list-background'>
-            {tasks&& tasks.map((task, idx)=>{
-                return <li className='task-item' key={idx}>{task}</li>
-            })}
-    </ul>
-  )
+    <>
+      <h3>{title}</h3>
+      <ul id="list-background">
+        {tasks &&
+          tasks.map((task, idx) => {
+            return (
+              <li className="task-item" key={idx}>
+                {task} <button onClick={()=>{}}><MdDelete color="red"/></button>
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
 }
 
-export default ListBackground
+export default ListBackground;
